940c5d711012ef4c5ac40a2fe7d97aa67a7d9a7e
masahirot <13981959+pentam19@users.noreply.github.com>
2021-05-22 11:00:49 +0900

    SampleCode

---
+++ SomeConsoleApplication/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace SomeConsoleApplication
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+
+        public void ArgTest(string str)
+        {
+            Console.WriteLine(str.Length);
+        }
+
+        public string GetTest(bool flg)
+        {
+            if (flg)
+            {
+                return "bbbbb";
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SomeConsoleApplication/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace SomeConsoleApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            var iAmTrue = true;
+            if (iAmTrue)
+            {
+                // Test1
+                Console.WriteLine("Call MyClass.ArgTest()");
+                //myClass.ArgTest("aaaaa");
+                myClass.ArgTest(null);
+
+                // Test2
+                Console.WriteLine("Call MyClass.GetTest()");
+                //string str = myClass.GetTest(true);
+                string str = myClass.GetTest(false);
+                Console.WriteLine(str.Length);
+
+                //
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static bool AlwaysReturnsTrue()
+        {
+            return true;
+        }
+
+        public static object Passthrough(object obj)
+        {
+            return obj;
+        }
+    }
+}
